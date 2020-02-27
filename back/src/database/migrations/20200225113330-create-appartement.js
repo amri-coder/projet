8ebbb5fb4b47e9fa542c0b751448be082229f3d1
amri debian <amri@debian>
2020-02-27 10:20:51 +0100

    back and front

--- back/src/database/migrations/20200225113330-create-appartement.js
+++ back/src/database/migrations/20200225113330-create-appartement.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       image:{
         allowNull: false,
-        type: Sequelize.STRING(150)
+        type: Sequelize.STRING
       },
       type: {
         allowNull: false,
